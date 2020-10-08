449bf713886ea961122220afeb514e00482c40bd
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-10-08 18:57:45 +0900

    2.3

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,19 +1,39 @@
 from rest_framework import serializers
-from users.serializers import TinyUserSerializer
+from users.serializers import UserSerializer
 from .models import Room
 
 
-class RoomSerializer(serializers.ModelSerializer):
+class ReadRoomSerializer(serializers.ModelSerializer):
 
-    user = TinyUserSerializer()
-
-    class Meta:
-        model = Room
-        fields = ("pk", "name", "price", "instant_book", "user")
-
-
-class BigRoomSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
 
     class Meta:
         model = Room
-        exclude = ()
+        exclude = ("modified",)
+
+
+class WriteRoomSerializer(serializers.Serializer):
+
+    name = serializers.CharField(max_length=140)
+    address = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    beds = serializers.IntegerField(default=1)
+    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
+    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
+    bedrooms = serializers.IntegerField(default=1)
+    bathrooms = serializers.IntegerField(default=1)
+    check_in = serializers.TimeField(default="00:00:00")
+    check_out = serializers.TimeField(default="00:00:00")
+    instant_book = serializers.BooleanField(default=False)
+
+    def create(self, validated_data):
+        return Room.objects.create(**validated_data)
+
+    def validate(self, data):
+        check_in = data.get("check_in")
+        check_out = data.get("check_out")
+        if check_in == check_out:
+            raise serializers.ValidationError(
+                "Not enough time between changes")
+        else:
+            return data
--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,40 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Room
-from .serializers import RoomSerializer, BigRoomSerializer
+from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
 
-class ListRoomsView(ListAPIView):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+class RoomsView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()[:5]
+        serializer = ReadRoomSerializer(rooms, many=True).data
+        return Response(serializer)
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = WriteRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            room = serializer.save(user=request.user)
+            room_serializer = ReadRoomSerializer(room).data
+            return Response(data=room_serializer, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class SeeRoomView(RetrieveAPIView):
-    queryset = Room.objects.all()
-    serializer_class = BigRoomSerializer
+class RoomView(APIView):
+
+    def get(self, request, pk):
+        try:
+            room = Room.objects.get(pk=pk)
+            serializer = ReadRoomSerializer(room).data
+            return Response(serializer)
+        except Room.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        pass
+
+    def delete(self, request):
+        pass
