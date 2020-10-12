08b96a241bef6b22a09e703f4f58c853533f0fc6
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-10-12 17:47:12 +0900

    2.10

--- users/views.py
+++ users/views.py
@@ -6,7 +6,18 @@ from rest_framework import status
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 from .models import User
-from .serializers import ReadUserSerializer, WriteUserSerializers
+from .serializers import UserSerializer
+
+
+class UsersView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(UserSerializer(new_user))
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MeView(APIView):
@@ -15,25 +26,16 @@ class MeView(APIView):
 
     def get(self, request):
         if request.user.is_authenticated:
-            return Response(ReadUserSerializer(request.user).data)
+            return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerializers(
+        serializer = UserSerializer(
             request.data, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response()
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class FavsView(APIView):
@@ -59,3 +61,12 @@ class FavsView(APIView):
             except Room.DoesNotExist:
                 pass
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
